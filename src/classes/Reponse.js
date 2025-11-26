c1e09676723ed36507f3cc825a6b9ed08c6dde17
Sacha <sacha.h.2005.51@gmail.com>
2025-11-26 20:32:15 +0100

    implement Reponse

---
+++ src/classes/Reponse.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {string} text
+ * @param {boolean} correct
+ */
+var Reponse = function(text, correct){
+    this.text = text;
+    this.correct = correct;
+};
+
+module.exports = Reponse;
