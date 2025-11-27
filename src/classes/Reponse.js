00957c2574519848f62c351c80f1828352649f73
Sacha <sacha.h.2005.51@gmail.com>
2025-11-27 10:28:45 +0100

    add feedback argument

--- src/classes/Reponse.js
+++ src/classes/Reponse.js
@@ -2,10 +2,12 @@
  *
  * @param {string} text
  * @param {boolean} correct
+ * @param {string} feedback
  */
-var Reponse = function(text, correct){
+var Reponse = function(text, correct, feedback){
     this.text = text;
     this.correct = correct;
+    this.feedback = feedback;
 };
 
 module.exports = Reponse;
