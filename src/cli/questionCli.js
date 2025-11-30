72735a8664e743c977ede9b5f431860fa456ce3f
Sacha <sacha.h.2005.51@gmail.com>
2025-11-30 12:36:35 +0100

    command view and seach read all datas

--- src/cli/questionCli.js
+++ src/cli/questionCli.js
@@ -6,51 +6,65 @@ const fs = require('fs');
 
 cli
     .command('view', 'display question')
-    .argument('<file>', 'The gift file to use')
     .argument('<id>', 'The question id')
     .action(({args, options, logger}) => {
-        fs.readFile(args.file, 'utf8', function (err,data) {
-            if (err) {
-                return logger.warn(err);
-            }
+        fs.readdir("./data/", (err, files) => {
+            files.forEach(file => {
+                const fileNameSplit = file.split('.');
+                if (fileNameSplit[fileNameSplit.length-1] === "gift") {
+                    fs.readFile(`data//${file}`, 'utf8', function (err,data) {
+                        if (err) {
+                            return logger.warn(err);
+                        }
 
-            const analyzer = new QuestionParser();
-            analyzer.parseGift(data);
-            // console.log(JSON.stringify(analyzer.parsedQuestion, null, 2));
+                        const analyzer = new QuestionParser();
+                        analyzer.parseGift(data);
+                        // console.log(JSON.stringify(analyzer.parsedQuestion, null, 2));
 
-            if (analyzer.errorCount === 0) {
-                let quest = analyzer.parsedQuestion.filter( (q) => {return q.id === args.id;});
+                        if (analyzer.errorCount === 0) {
+                            let quest = analyzer.parsedQuestion.filter( (q) => {return q.id === args.id;});
 
-                if (quest.length === 0) {
-                    logger.warn("Identifiant absent ou incorrect.")
-                } else {
-                    logger.info(JSON.stringify(quest, null, 2));
+                            if (quest.length === 0) {
+                                logger.warn(`${file}: Identifiant absent ou incorrect.`)
+                            } else {
+                                logger.info(`${file} : ${JSON.stringify(quest, null, 2)}`);
+                            }
+                        }
+                    });
                 }
-            }
+            });
         });
     })
 
     .command('search', 'search question')
-    .argument('<file>', 'The gift file to use')
     .argument('<text>', 'search by texte')
     .action(({args, options, logger}) => {
-        fs.readFile(args.file, 'utf8', function (err,data) {
-            if (err) {
-                return logger.warn(err);
-            }
+        fs.readdir("./data/", (err, files) => {
+            files.forEach(file => {
+                const fileNameSplit = file.split('.');
+                if (fileNameSplit[fileNameSplit.length - 1] === "gift") {
+                    fs.readFile(`data//${file}`, 'utf8', function (err, data) {
+                        if (err) {
+                            return logger.warn(err);
+                        }
 
-            const analyzer = new QuestionParser();
-            analyzer.parseGift(data);
+                        const analyzer = new QuestionParser();
+                        analyzer.parseGift(data);
 
-            if (analyzer.errorCount === 0) {
-                let quest = analyzer.parsedQuestion.filter( (q) => {return q.id.includes(args.text) || q.questionType.includes(args.text) || q.text.includes(args.text)});
+                        if (analyzer.errorCount === 0) {
+                            let quest = analyzer.parsedQuestion.filter((q) => {
+                                return q.id.includes(args.text) || q.questionType.includes(args.text) || q.text.includes(args.text)
+                            });
 
-                if (quest.length === 0) {
-                    logger.warn("Aucun résultat trouvé.")
-                } else {
-                    logger.info(JSON.stringify(quest, null, 2));
+                            if (quest.length === 0) {
+                                logger.warn(`${file} : Aucun résultat trouvé.`)
+                            } else {
+                                logger.info(`${file} : ${JSON.stringify(quest, null, 2)}`);
+                            }
+                        }
+                    });
                 }
-            }
+            });
         });
     })
 
